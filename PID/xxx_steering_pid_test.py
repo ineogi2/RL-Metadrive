54dc7d392338b1e78e2c3f9093fd2d997166c6bb
HyeokJin Kwon <ineogi2@gmail.com>
2022-08-12 00:34:00 +0900

    0812 update

--- PID/xxx_steering_pid_test.py
+++ PID/xxx_steering_pid_test.py
@@ -5,11 +5,11 @@ sys.path.append("/home/ineogi2/RL-Lab/metadrive")
 
 from metadrive import SafeMetaDriveEnv
 
-Kp_list = [1]
-Ki_list = [0.001, 0.002, 0.003, 0.004]
-Kd_list = [0.001, 0.01, 0.02, 0.03, 0.004]
+Kp_list = [1, 2, 3, 4, 5]
+Ki_list = [0.001, 0.01, 0.1, 1]
+Kd_list = [0.001, 0.01, 0.1, 1]
 
-def dir_control(cur_err, prv_err, err_sum, dt, Kp, Ki, Kd):
+def steering_control(cur_err, prv_err, err_sum, dt, Kp, Ki, Kd):
     # hyperparameter
 
 
@@ -17,13 +17,13 @@ def dir_control(cur_err, prv_err, err_sum, dt, Kp, Ki, Kd):
     upper_acc = 0.3
     lower_acc = -0.3
 
-    acc = Kp*cur_err + Ki*dt*err_sum + Kd*(cur_err-prv_err)/dt
+    steering = Kp*cur_err + Ki*dt*err_sum + Kd*(cur_err-prv_err)/dt
 
     # if abs(cur_err) < 3:
     #     if acc < lower_acc: acc = lower_acc
         # elif acc > lower_acc: acc = upper_acc
 
-    return acc
+    return steering
 
 env=SafeMetaDriveEnv(dict(
     # environment_num=1,
@@ -32,49 +32,49 @@ env=SafeMetaDriveEnv(dict(
 ))
 env.reset()
 
-speed_aim = 40
+steering_aim = 0.5
 err_sum = 0
 cur_err = 0
 prv_err = 0
-init_speed = 0
+init_steering = 0
 prv_time = time.time()
 
-speed_list = []
+steering_list = []
 
 obs, reward, done, info = env.step([0,0])
 
 for Kp in Kp_list:
     for Ki in Ki_list:
         for Kd in Kd_list:
-            while (not done) & (len(speed_list)<=100):
+            while (not done) & (len(steering_list)<=100):
                 cur_time = time.time()
-                cur_speed = info['vehicle_speed']
-                speed_list.append(cur_speed)
+                cur_steering = info['steering']
+                steering_list.append(cur_steering)
 
-                print("\ncur_speed : ",cur_speed)
+                print("\ncur_steering : ",cur_steering)
                 # print(info)
 
                 dt = cur_time - prv_time
-                cur_err = speed_aim - cur_speed
+                cur_err = steering_aim - cur_steering
                 err_sum += cur_err
                 # print("cur_err : ",cur_err," / prv_err : ",prv_err," / err_sum : ",err_sum)
 
-                acc = speed_control(cur_err, prv_err, err_sum, dt, Kp, Ki, Kd)
+                steering = steering_control(cur_err, prv_err, err_sum, dt, Kp, Ki, Kd)
                 # print("acc : ",acc)
-                obs, reward, done, info = env.step([0,acc])
+                obs, reward, done, info = env.step([steering,0.1])
 
                 prv_time = cur_time
                 prv_err = cur_err
                 env.render()
 
-            plt.plot(speed_list[:100],'b') # x_range=plt.xlim()
-            plt.hlines(speed_aim,0,99,colors='red')
+            plt.plot(steering_list[:100],'b') # x_range=plt.xlim()
+            plt.hlines(steering_aim,0,99,colors='red')
             file_name = 'Kp='+str(Kp)+" & Ki="+str(Ki)+" & Kd="+str(Kd)+'.png'
             plt.savefig(file_name) # plt.show()
             plt.close()
 
             # input("Press Enter to continue...")
-            speed_list = []; err_sum = 0; cur_err = 0; prv_err = 0; cur_speed = 0
+            steering_list = []; err_sum = 0; cur_err = 0; prv_err = 0; cur_steering = 0
 
             env.reset(); done=False; obs, reward, done, info = env.step([0,0])
         
