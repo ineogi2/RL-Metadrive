74c5722d738a4cad372c75ffda4afa24d25af746
HyeokJin Kwon <ineogi2@gmail.com>
2022-09-13 23:55:35 +0900

    0913 update

--- PID/PID_controller.py
+++ PID/PID_controller.py
@@ -1,6 +1,8 @@
 import math
 import numpy as np
 
+from CPO.torch.agent import clip
+
 class PID_controller:
     def __init__(self, info, speed_gain=[1,0,0.01], steering_gain=[0.7,0,0]):
         self.speed_gain=speed_gain
@@ -16,12 +18,6 @@ class PID_controller:
         self.vehicle_length=info["vehicle_length"]
 
         self._update(info)
-        # self.lane_num=self.lane_to_left//self.lane_width+1
-        # self.heading=math.degrees(math.asin(info['vehicle_heading']))
-        # self.speed=info['vehicle_speed']
-        # self.speed_aim=25
-        # (self.lane_curved,self.lane_width,self.lane_to_left)=info['vehicle_heading_sine']
-        # self.lane_num=self.lane_to_left//self.lane_width+1 # (0 for straight 1 for curve, lane width, distance to left side)
 
     def _update(self, info):
         self.speed = info['vehicle_speed']
@@ -34,11 +30,9 @@ class PID_controller:
 
         if self.lane_change_sign:
             if self.aim_lane_num == self.cur_lane_num:
-                # print(_cur_lateral,self.lane_width)
                 self.lane_change_sign=0
 
         _lateral_aim = (self.aim_lane_num+0.5)*self.lane_width
-        # print(_lateral_aim, _cur_lateral)
         self.speed_err = self._update_err(self.speed_err, self.speed, self.speed_aim)
         self.dir_err = self._update_err(self.dir_err, _cur_lateral, _lateral_aim)
         
@@ -59,23 +53,38 @@ class PID_controller:
             input += pid_gain[i]*err[i]
         return input
 
-    def vehicle_control(self):
-        if self.lane_change_sign:
-            input = self.lane_change()
-        else: 
+    def vehicle_control(self, clipped_decision):
+        clipped_decision = list(clipped_decision)
+        tmp = max(clipped_decision)
+        num = clipped_decision.index(tmp)
+
+        if num==0:
             input = self.lane_keeping()
-        # print(input[0])
+        elif num==1:
+            input = self.lane_keeping()
+            input[1] = -0.7
+        elif num==2:
+            self.go_left()
+            input = self.lane_change()
+        elif num==3:
+            self.go_right()
+            input = self.lane_change()
+
+        # if self.lane_change_sign:
+        #     input = self.lane_change()
+        # else: 
+        #     input = self.lane_keeping()
         return input
 
     def lane_keeping(self):
-        print('\nlane keeping')
+        # print('\nlane keeping')
         if abs(self.lane_change_count)>0:
             if self.dir_err[0]*self.dir>0:
-                print('1')
+                # print('1')
                 steering_angle = self.heading*0.1
                 self.lane_change_count-=1
             else:
-                print('2')
+                # print('2')
                 steering_angle = -self.heading*0.001
         else:
             steering_angle = -self._pid_result(self.steering_gain, self.dir_err)
@@ -86,9 +95,8 @@ class PID_controller:
         return input
 
     def lane_change(self):
-        print('\nlane changing')
+        # print('\nlane changing')
         steering_angle = -self._pid_result(self.steering_gain, self.dir_err)
-        # steering_angle = np.clip(steering_angle, self.heading*0.1-0.3,self.heading*0.1+0.3)
         if not self.lane_curved:
             steering_angle=np.clip(steering_angle,-0.09,0.09)
         else:
